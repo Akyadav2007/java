b3236fc0f40b6e6284c260b20c1aa20f6c39409d
Aman kumar <Aman_0607@outlook.com>
2026-03-29 11:42:01 +0530

    Add files via upload

---
+++ chefandtrianglepizza.java
@@ -0,0 +1,20 @@
+    import java.util.Scanner;
+    public class chefandtrianglepizza {
+
+
+  public static void main(String args[]){
+    Scanner sc=new Scanner (System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int c=sc.nextInt();
+    if(a==b && b==c){
+      System.out.println("Equilateral");
+    } else if(a==b||b==c||c==a){
+      System.out.println("Isosceles");
+    }else{
+      System.out.println("Scalene");
+    }
+    sc.close();
+    }
+}
+
